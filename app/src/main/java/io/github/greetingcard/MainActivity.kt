1afca289eca52bd577fa8536e288d57b902b88b6
Jesus <jesusgil1993@outlook.com>
2023-04-02 03:48:42 -0700

    new image logo and color background added

--- app/src/main/java/io/github/greetingcard/MainActivity.kt
+++ app/src/main/java/io/github/greetingcard/MainActivity.kt
@@ -3,19 +3,25 @@ package io.github.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.JesusGil.R
 import io.github.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Residence(address = "Address: ")
+                    Residence(address = "Fresh Prince of Bel Air, 251 N Bristol Ave, Los Angeles, CA 90049")
                 }
             }
         }
@@ -37,13 +43,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable 
 fun Residence(address: String) {
-    Surface(color = Color.White) {
+    val logo = painterResource(id = R.drawable.android_logo)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(Color.Black)
+    ) {
+        Image(
+            painter = logo,
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .padding(32.dp)
+                .size(150.dp)
+                .border(3.dp, color = Color.White),
+        )
         Text(
-            text = "Address: $address",
+            text = "Address: \n$address",
             Modifier.padding(32.dp),
-            color = Color.Black,
+            color = Color.White,
             fontWeight = FontWeight.Bold,
             fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Justify,
         )
     }
 }
